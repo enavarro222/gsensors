fec28b8651bbaeadafcc83d10ea0610068bab487
Emmanuel N <enavarro222@gmail.com>
2015-07-27 00:47:12 +0200

    add utils to log traceback

--- gsensors/utils.py
+++ gsensors/utils.py
@@ -1,10 +1,10 @@
 #-*- coding:utf-8 -*-
 import logging
+import sys
 
-def get_logger():
-    logger = logging.getLogger()
+def get_logger(level=logging.DEBUG):
+    logger = logging.getLogger("gsensors")
     ## logger
-    level = logging.DEBUG
     logger.setLevel(level)
     # create console handler with a higher log level
     ch = logging.StreamHandler()
@@ -15,3 +15,37 @@ def get_logger():
     # add the handlers to the logger
     logger.addHandler(ch)
     return logger
+
+
+# see http://stackoverflow.com/questions/13210436/get-full-traceback/13210518#13210518
+
+class FauxTb(object):
+    def __init__(self, tb_frame, tb_lineno, tb_next):
+        self.tb_frame = tb_frame
+        self.tb_lineno = tb_lineno
+        self.tb_next = tb_next
+
+def current_stack(skip=0):
+    try: 1/0
+    except ZeroDivisionError:
+        f = sys.exc_info()[2].tb_frame
+    for i in xrange(skip + 2):
+        f = f.f_back
+    lst = []
+    while f is not None:
+        lst.append((f, f.f_lineno))
+        f = f.f_back
+    return lst
+
+def extend_traceback(tb, stack):
+    """Extend traceback with stack info."""
+    head = tb
+    for tb_frame, tb_lineno in stack:
+        head = FauxTb(tb_frame, tb_lineno, head)
+    return head
+
+def full_exc_info():
+    """Like sys.exc_info, but includes the full traceback."""
+    t, v, tb = sys.exc_info()
+    full_tb = extend_traceback(tb, current_stack(1))
+    return t, v, full_tb
